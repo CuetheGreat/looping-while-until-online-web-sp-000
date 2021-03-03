8c13d3bc92b727ddca5c40e27b96df917c297e3e
Clayon Stewart <c.stewart89@gmail.com>
2021-03-03 02:22:41 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,9 +2,9 @@ def using_until
   levitation_force = 6
   #your code here
   counter = 0
-  until counter == levitation_force
-    puts "Forces are not equal"
-    counter += 1
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
   end
 end
 
