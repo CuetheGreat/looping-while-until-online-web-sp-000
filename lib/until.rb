89935dde76cbdc520c2fe17b8c579ac0a3d38b4c
Clayon Stewart <c.stewart89@gmail.com>
2021-03-03 02:17:56 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
   levitation_force = 6
   #your code here
-    
+  counter = 0
+  until counter == levitation_force
+    puts "Forces are not equal"
+    counter += 1
+  end
 end
 
