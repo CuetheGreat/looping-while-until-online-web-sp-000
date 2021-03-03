9e34ea6e9a3611ecb48d0ab08fec5c66f2b4929c
Clayon Stewart <c.stewart89@gmail.com>
2021-03-03 02:19:15 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,11 @@ def using_while
   levitation_force = 6
 	
   #your code here
+  counter = 0
+  while counter < levitation_force
+    puts "Counter is less than #{levitation_force}"
+    counter += 1
+  end
 end
 
 
